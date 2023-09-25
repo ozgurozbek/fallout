7cce3f9dc2e8d7eb1e997f5c815d51db7adea288
Özgür Özbek <43455645+ozgurozbek@users.noreply.github.com>
2023-09-25 17:54:01 +0100

    Update Information page

--- pages/Information.tsx
+++ pages/Information.tsx
@@ -16,13 +16,7 @@ export default function InformationPage() {
           contentProps={{
             title: "Intro",
             text: [
-              'Long before their battles, gods were once "All," harnessing the energy and magic of reality. However, their egos led to conflicts and the need to determine superiority. Lesser creatures desired an end to the fighting, but gods, being immortal and unforgiving, couldn\'t accept another "Perfect Being\'s" actions.',
-              "Eventually, prayers were answered, and the gods ceased fighting, but this created a worse situation. They stopped communicating and individually created humans from Mceeju, who, despite their differences, served the gods. The gods made peace with their divergences and established various races within the Astral Sea.",
-              "Humans, lacking intelligence and divine guidance, possessed magic but often perished due to imperfections. Their deaths released magic into the universe. The gods accepted their punishment and judged themselves, losing their perfect nature and becoming distinct from one another.",
-              "The gods continued to create humans, who exhibited predictable yet mysterious traits. They showed mercy and guided some humans towards greatness, rewarding them with demigod status. However, such accomplishments became scarce over time.",
-              "Despite sharing the same creators, races clashed or multiplied uncontrollably, losing control of the Weave and facing an unstoppable force: Mceeju.",
-              "Mceeju divided the Astral Sea, creating the Material Planes and the Inner Worlds while isolating the Elemental Chaos with a dimensional barrier. Races were divided, and even the gods' domains were split. The Overworld, Underworld, and Outer Worlds emerged.",
-              "With newfound freedom, the gods led by Mceeju's order. Some stayed united, some pursued separate paths, and others descended to the Material Planes.",
+              "In the alternate history of Fallout: Istanbul, the world follows a divergent path from the mid-20th century, leading to a post-apocalyptic wasteland following the Great War of 2077. Istanbul, miraculously spared from nuclear devastation but hit by a series of catastrophic events, becomes a beacon of hope and civilization in the region. As survivors struggle to adapt, numerous factions vie for control, power, and the remnants of a shattered world.",
             ],
           }}
         />
@@ -30,56 +24,29 @@ export default function InformationPage() {
         <SimpleContent
           contentProps={{
             image: {
-              src: "./wheel.png",
-              alt: "Wheel Cosmology",
+              src: "./map.png",
+              alt: "Istanbul Map",
             },
-            title: "Dimensional Cosmology",
+            title: "Fallout: Istanbul",
             text: [
-              "The Wheel Cosmology in Teothe is a conceptual framework that organizes the multiverse into a series of interconnected planes and realms. It envisions existence as a great wheel with various planes revolving around a central hub, the Marble. The cosmology encompasses the Far Realms, Outer Planes representing different alignments, Inner Planes embodying elemental forces, the Astral Sea connecting all planes, and additional planes like the Feywild and Shadowfell that mirror and intersect with the Material Plane. This cosmological model provides a structured and interconnected backdrop for adventures and campaigns, offering a vast array of realms and dimensions for exploration and storytelling.",
-              "Material Plane: The plane of existence where most D&D adventures take place. It is a diverse and varied world, consisting of continents, seas, and skies. It is the realm inhabited by mortals, and it serves as a central hub connecting with other planes.",
-              "Feywild: Also known as the Plane of Faerie, it is a vibrant and magical reflection of the Material Plane. It is home to fey creatures, such as fairies, elves, and other enchanted beings. Time and nature behave differently in the Feywild.",
-              "Shadowfell: A dark and gloomy plane that mirrors the Material Plane, associated with darkness, decay, and negative emotions. It is a realm of shadows and is inhabited by undead creatures and other dark entities. Time and nature behave differently in the Shadow plane.",
-              "Ethereal Plane: A misty and insubstantial realm that coexists with the Material Plane. It is often accessed through magical means and is used for travel and interaction with ethereal beings and objects.",
-              "Elemental Planes: In addition to the Inner Planes, there are also Para-Elemental Planes and Quasi-Elemental Planes. The Para-Elemental Planes lie between two adjacent Inner Planes and contain a blend of elemental energies. The Quasi-Elemental Planes exist between the Elemental Planes and combine elemental forces with other phenomena like ash, steam, ice, and more.",
-              "Inner Planes: Elemental planes that embody the raw forces of nature. They include the Elemental Plane of Fire, Elemental Plane of Water, Elemental Plane of Air, and Elemental Plane of Earth. Each plane is abundant in its corresponding elemental energy.",
-              "Outer Planes: A collection of planes representing different moral and ethical alignments. These planes include the Overseer realms. They are home to powerful deities and beings embodying these alignments.",
-              "Astral Sea: The plane that connects all the other planes, serving as a vast void of silvery mist. It is traversed by the Astral Projection and acts as a conduit between different realms. It is also home to astral creatures.",
-              "Far Realms: A plane of existence that lies beyond the known multiverse, characterized by eldritch and incomprehensible entities. It is a realm of madness and alien horrors.",
-              "These summaries provide a brief overview of each component within the Dimensional Cosmology of Teothe, offering a glimpse into the diverse and interconnected planes that players and characters may explore during their adventures.",
+              "Welcome to the post-apocalyptic realm of Fallout, a world scarred by nuclear devastation and steeped in darkness and uncertainty. The Fallout Cosmology is a conceptual framework that organizes this grim and interconnected multiverse, offering a glimpse into the bleak dimensions that define this universe.",
+              "The wasteland is the primary place where Fallout adventures take place. It's a harsh and unforgiving world, filled with irradiated landscapes, ruined cities, and the constant struggle for resources. The Wasteland is inhabited by wastelanders, mutated creatures, and various factions vying for control.",
+              "Particularly hazardous region within the Wasteland, the Glowing Sea, is heavily irradiated and home to some of the most dangerous creatures and anomalies in the Fallout universe. It is a place of extreme danger and mystery.",
+              "There are also particularly weird anomalies, such as ghosts, dream-like beings, immortals, and the cyberspace that are almost always explored during adventures.",
             ],
           }}
         />
         <Divider />
         <SimpleContent
           contentProps={{
-            title: "Gate Keepers",
+            title: "Pre-War Turkey",
             text: [
-              "All take their power from Mceeju so all are close to gods in power.",
-              "Images of Raajod are the gate-keepers of Overworld Vocals that are in Outer Realms for many marbles.",
-              "Images of Raxy are the gate-keepers of Underworld Vocals that are in Outer Realms for many marbles.",
-              "Onposufm or Wefhiza maintains order in the Astral Sea, but is believed to be the guardian of accidents.",
-            ],
-          }}
-        />
-        <Divider />
-        <SimpleContent
-          contentProps={{
-            title: "Ancients",
-            text: [
-              "All take their power from Mceeju so all are close to gods in power.",
-              "An old civilization that has evolved and found great meaning in both magic and immortality. They made immortality possible and destroyed every meaning they've found within it. With their last magic, they left a warning for future, unfortunately too difficult to grasp, lacking their understanding of magic, life and time. They managed to process light, and believed to achieve a hive-mind society. Nobody knows how they disappeared.",
-              "Their offsprings from humans tend to get a disoriented body with partial ancient bodyparts. They live for about a millenia years without aging beyond their thirties, body-wise. They are sterile.",
-              "The last Ancient Relic dwindled out of existance, causing the Blue Era in Teothe. Steram Wayogo, known as the last half-ancient embarked on a journey across countless marbles sealing Ancient Gates, that once provided a unique magical flow to the Material Planes.",
-            ],
-          }}
-        />
-        <Divider />
-        <SimpleContent
-          contentProps={{
-            title: "Domains",
-            text: [
-              "Nature, Tempest, Trickery, Death, Knowledge, War, Life and Light.",
-              "Pantheon deities use fundamental domains. Gameplay-wise, other domains like Endless domains (destruction, destiny, despair, desire, delirium, dream), Broad domains (decay, drama, fabrication, fate, time, distance, potential) or ones introduced in XGE (forge, grave), or UA (order) are welcome.",
+              "In the mid-20th century, the political landscape of Turkey saw Adnan Menderes running for election but ultimately failing to secure victory. This period weakened the religious aspects of Turkey's governance, setting the stage for a pivotal shift. It was a time when Turkey played a crucial role in the post-war recovery efforts across Europe and the Middle East. However, political tensions and an evolving authoritarian climate led to Menderes's eventual execution in the early 1960s, leaving an indelible mark on the nation.",
+              "Turkey's strategic location in the 1960s and beyond made it a critical battleground for resource-rich areas. This led to conflicts over access to valuable resources, resulting in political instability and economic turmoil. Turks, guided by various secular governments, began aligning themselves more closely with the European Commonwealth, seeking stability and prosperity in an unstable world.",
+              "By the 1980s and 1990s, Turkey had solidified its ties with the European Commonwealth, becoming a key member and reaping the benefits of economic and military support. This period saw Turks emerging as skilled traders and negotiators, drawing upon their historical aptitude for diplomacy and commerce. They also made significant strides in technological advancements, aligning seamlessly with the values of the European Commonwealth.",
+              "The 2000s marked a turning point as Turkey became a regional leader in advanced weaponry, infrastructure, and technological innovation. This further modernization of the country was accompanied by the initiation of Turkey's nuclear program, backed by the European Commonwealth. As the world moved into the 2030s, Turkey became a hub for research and development in robotics, nuclear power, and cybernetics, attracting talent from the European Commonwealth.",
+              "As tensions escalated and Turkey found itself enmeshed in regional conflicts driven by resource scarcity, the looming specter of nuclear warfare turned the nation into a potential battleground for major world powers. In response to this precarious situation, the deeply ingrained 'Family Structure' of Turkish society catalyzed the creation of a Turkish branch of VaultTec known as Vatandaş Omuzlayan Lütufkar Toplum Teknolojileri, or VOLT-TEK. Within the confines of their vault, Turks prepared diligently for the impending nuclear threats, determined to preserve their cultural heritage and ensure their survival in a world on the precipice of disaster.",
+              "In the post-2077 wasteland, Istanbul miraculously escaped nuclear devastation, becoming a beacon of hope and civilization. Despite the cataclysmic events, the surviving Turks, in collaboration with the Turkish branch of VaultTec (Vatandaş Omuzlayan Lütufkar Toplum Teknolojileri - VOLTTEK), rallied to rebuild and preserve their cultural heritage. Their history, characterized by resilience, innovation, and enduring ties to the European Commonwealth, continues to shape their identity as resourceful and adaptable survivors in the post-apocalyptic world.",
             ],
           }}
         />
